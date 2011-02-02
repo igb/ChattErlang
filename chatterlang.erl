e1a7b7ee0c5be07b6e02b04041ba360f00b80a32
Ian Brown <igb@hccp.org>
2011-02-02 00:19:36 -0800

    update_status and post_link implemented

--- chatterlang.erl
+++ chatterlang.erl
@@ -11,11 +11,19 @@ login (Username, Password, SecurityToken, Endpoint)->
     
 
 post_link(Url, Description, SessionId, Endpoint)->
-    ok.
+    Id=get_current_user_id(SessionId, Endpoint),
+    PostLinkSobject=[
+		     {"type", "string", "FeedPost"},
+		     {"ParentId", "string", Id},
+		     {"LinkUrl", "string", Url}
+		    ],
+    case Description of 
+	"" -> sfdc:create(PostLinkSobject, SessionId, Endpoint);
+	_ ->  sfdc:create(lists:flatten([PostLinkSobject,{"Title", "string", Description}]), SessionId, Endpoint)
+    end.
 
 post_status(Status, SessionId, Endpoint)->
-    UserInfo=sfdc:get_user_info(SessionId, Endpoint),
-    {_,_,Id}=lists:keyfind("userId", 1, UserInfo),
+    Id=get_current_user_id(SessionId, Endpoint),
     UpdateStatusSobject=[
 			 {"type", "string", "User"},
 			 {"Id", "string", Id},
@@ -25,4 +33,7 @@ post_status(Status, SessionId, Endpoint)->
     sfdc:update(UpdateStatusSobject, SessionId, Endpoint).
 
     
-
+get_current_user_id(SessionId, Endpoint)->
+    UserInfo=sfdc:get_user_info(SessionId, Endpoint),
+    {_,_,Id}=lists:keyfind("userId", 1, UserInfo),
+    Id.
